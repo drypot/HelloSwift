ed867551b00dffaf8661a38f24a48cc56b07b302
drypot <drypot@gmail.com>
2025-11-14 18:33:43 +0900

    add testWhenExposedPropertyChangedMultipleTimes

--- Tests/HelloSwiftTests/Observation/ObservableTests.swift
+++ Tests/HelloSwiftTests/Observation/ObservableTests.swift
@@ -35,8 +35,30 @@ struct ObservableTests {
             logger.log(2)
         }
 
-        // 노출된 프로퍼티가 업데이트되면 onChange 가 호출된다.
         pet.name = "max juior"
+
+        // 노출된 name 프로퍼티가 업데이트되어 onChange 가 호출되었다.
+
+        #expect(logger.result() == [1, 2])
+    }
+
+    @Test func testWhenExposedPropertyChangedMultipleTimes() async throws {
+        let logger = SimpleLogger<Int>()
+
+        let pet = Pet(name: "max", age: 7)
+
+        withObservationTracking {
+            _ = pet.name
+            logger.log(1)
+        } onChange: {
+            logger.log(2)
+        }
+
+        pet.name = "max juior"
+        pet.name = "max juior again"
+
+        // 노출된 name 프로퍼티가 업데이트되어 onChange 가 호출되었다.
+        // 한번만 호출되었다.
 
         #expect(logger.result() == [1, 2])
     }
@@ -53,8 +75,9 @@ struct ObservableTests {
             logger.log(2)
         }
 
-        // 노출되지 않은 프로퍼티가 업데이트되면 호출되지 않는다.
         pet.age = 2
+
+        // name 프로퍼티가 노출되었는데 age 프로퍼티를 변경하니 onChange 가 호출되지 않았다.
 
         #expect(logger.result() == [1])
     }
