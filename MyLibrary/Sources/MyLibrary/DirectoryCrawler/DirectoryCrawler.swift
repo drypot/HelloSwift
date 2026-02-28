9e0f182657b5d635f439d0c6609aaf88ec5bd06b
drypot <drypot@gmail.com>
2026-03-01 02:32:39 +0900

    update DirectoryCrawler

--- MyLibrary/Sources/MyLibrary/DirectoryCrawler/DirectoryCrawler.swift
+++ MyLibrary/Sources/MyLibrary/DirectoryCrawler/DirectoryCrawler.swift
@@ -6,15 +6,54 @@
 //
 
 import Foundation
+import UniformTypeIdentifiers
 
-public class Folder {
-    public var name: String
+public final class Folder: Identifiable, Comparable, Hashable {
     public var url: URL
+    public var name: String
     public var folders: [Folder]?
+
+    public var id: URL { url }
 
     public init(url: URL) {
         self.url = url
         self.name = url.lastPathComponent
+    }
+
+    public static func == (lhs: Folder, rhs: Folder) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    public static func < (lhs: Folder, rhs: Folder) -> Bool {
+        return lhs.name < rhs.name
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+public final class File: Identifiable, Comparable, Hashable {
+    public let url: URL
+    public let name: String
+
+    public var id: URL { url }
+
+    public init(url: URL) {
+        self.url = url
+        self.name = url.lastPathComponent
+    }
+
+    public static func == (lhs: File, rhs: File) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    public static func < (lhs: File, rhs: File) -> Bool {
+        return lhs.name < rhs.name
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
     }
 }
 
